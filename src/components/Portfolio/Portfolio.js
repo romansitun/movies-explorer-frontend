f5d94e3ba6cc7af0b028f6df67fe2a26a84733fa
Situn Roman <romansitun@yandex.ru>
2022-10-10 19:24:24 +0500

    ...

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -9,15 +9,15 @@ function Portfolio() {
     <h2 className='portfolio__title'>Портфолио</h2>
     <ul className='portfolio__projects'>
       <li className='portfolio__project'>
-        <p className='portfolio__name'>Статичный сайт</p>
+        <a className='portfolio__name app__link' href='https://github.com/romansitun/how-to-learn' target='_blank' rel='noopener noreferrer'>Статичный сайт</a>
         <a className='portfolio__link app__link' href='https://github.com/romansitun/how-to-learn' target='_blank' rel='noopener noreferrer'><img className='portfolio__pic' src={arrow} alt='Ссылка на проект со статичным сайтом'/></a>
       </li>
       <li className='portfolio__project'>
-        <p className='portfolio__name'>Адаптивный сайт</p>
+        <a className='portfolio__name app__link' href='https://github.com/romansitun/russian-travel' target='_blank' rel='noopener noreferrer'>Адаптивный сайт</a>
         <a className='portfolio__link app__link' href='https://github.com/romansitun/russian-travel' target='_blank' rel='noopener noreferrer'><img className='portfolio__pic' src={arrow} alt='Ссылка на проект с адаптивным сайтом'/></a>
       </li>
       <li className='portfolio__project'>
-        <p className='portfolio__name'>Одностраничное приложение</p>
+        <a className='portfolio__name app__link' href='https://github.com/romansitun/react-mesto-api-full' target='_blank' rel='noopener noreferrer'>Одностраничное приложение</a>
         <a className='portfolio__link app__link' href='https://github.com/romansitun/react-mesto-api-full' target='_blank' rel='noopener noreferrer'><img className='portfolio__pic' src={arrow} alt='Ссылка на проект с одностраничным приложением'/></a>
       </li>
     </ul>
